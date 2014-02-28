46d579a763630f2ffd41ae1f67106afa1b889031
Whitney Young <wbyoung@fadingred.com>
2014-02-28 14:31:46 -0800

    Verify the files that are bundled together exist.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-var Glob = require('glob').Glob,
+var glob = require('glob'),
     fs = require('fs'),
     path = require('path'),
     _ = require('lodash'),
     useref = require('useref'),
     gulp = require('gulp'),
+    gutil = require('gulp-util'),
     gulpif = require('gulp-if'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
@@ -23,7 +24,7 @@ module.exports = function(globs, opt){
     }
 
     return function () {
-        return new Glob(globs, opt, function (err, files) {
+        return new glob.Glob(globs, opt, function (err, files) {
             _.forEach(files, function (p) {
                 var assets = useref(fs.readFileSync(path.normalize(p), { encoding: 'utf-8'}))[1];
                 var css = assets.css;
@@ -36,9 +37,20 @@ module.exports = function(globs, opt){
                     return paths;
                 }
 
+                function verifyPath(paths) {
+                    _.forEach(paths, function (val, i) {
+                        var isGlob = glob.sync(val).length > 1;
+                        if (!isGlob && !fs.existsSync(val)) {
+                            gutil.log('[bundle]' + gutil.colors.red('Warning: no file matches ' + val));
+                        }
+                    });
+                    return paths;
+                }
+
                 _.forEach(css, function (paths, name) {
                     if (name !== '' && paths.length) {
                         prefixPath(paths);
+                        verifyPath(paths);
 
                         return gulp.src(paths)
                             .pipe(concat(path.basename(name)))
@@ -50,6 +62,7 @@ module.exports = function(globs, opt){
                 _.forEach(js, function (paths, name) {
                     if (name !== '' && paths.length) {
                         prefixPath(paths);
+                        verifyPath(paths);
 
                         return gulp.src(paths)
                             .pipe(concat(path.basename(name)))
